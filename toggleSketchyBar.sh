ef708e7816fbf018a7aba6726e4ddae2e2796a6e
Amal B Francis <amal.b@diagnal.com>
2025-05-30 13:13:16 +0530

    toggle sketchybar changes

--- toggleSketchyBar.sh
+++ toggleSketchyBar.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 toggle_padding=$(yabai -m query --windows --window | jq '.frame.y')
-if [[ $toggle_padding == 40.0000 ]]; then
+if [[ $toggle_padding == 35.0000 ]]; then
 	yabai -m config top_padding 10 
 	sketchybar --bar hidden=toggle
-elif [ "$toggle_padding" = "10.0000" ]; then
-	yabai -m config top_padding 40 
+elif [ $toggle_padding = "10.0000" ]; then
+	yabai -m config top_padding 35 
 	sketchybar --bar hidden=toggle
 fi
