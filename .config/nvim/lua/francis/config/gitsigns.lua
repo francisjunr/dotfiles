d82660f1b7bdab8cc1fc783ec219eccf69a75609
Amal B Francis <amalbfrancis@Amals-MacBook-Pro-2.local>
2026-01-07 21:49:34 +0530

    gitsigns plugin and its config updated

--- .config/nvim/lua/francis/config/gitsigns.lua
+++ .config/nvim/lua/francis/config/gitsigns.lua
@@ -48,7 +48,7 @@ local function on_attach(bufnr)
   map("n", "<leader>hp", gs.preview_hunk_inline, { desc = "Preview hunk" })
   map("n", "<leader>hb", function()
     gs.blame_line({ full = true })
-  end, {desc = "blame"})
+  end, { desc = "blame" })
   map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle current line blame" })
   map("n", "<leader>hd", gs.diffthis, { desc = "Diff split" })
   map("n", "<leader>hD", function()
@@ -60,42 +60,53 @@ local function on_attach(bufnr)
   map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 end
 
-gitsigns.setup({
+require('gitsigns').setup {
   on_attach = on_attach,
   signs = {
-    add = { text = "+" },
-    change = { text = "~" },
-    delete = { text = "_" },
-    topdelete = { text = "‾" },
-    changedelete = { text = "~" },
-    untracked = { text = "┆" },
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
   },
-  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = false,    -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false,   -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  signs_staged = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable = true,
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
-    follow_files = true,
+    follow_files = true
   },
-  attach_to_untracked = true,
+  auto_attach = true,
+  attach_to_untracked = false,
   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
     ignore_whitespace = false,
+    virt_text_priority = 100,
+    use_focus = true,
   },
-  current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
   max_file_length = 40000, -- Disable if file is longer than this (in lines)
   preview_config = {
     -- Options passed to nvim_open_win
-    border = "single",
-    style = "minimal",
-    relative = "cursor",
+    style = 'minimal',
+    relative = 'cursor',
     row = 0,
-    col = 1,
+    col = 1
   },
-})
+}
